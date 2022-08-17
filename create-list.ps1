26e25d87d53cec1fbfab6715a4a2f9c0a0d6f844
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-08-17 13:46:46 -0500

    Added iCSM TPID now required, indexed, and enforced unique

--- create-list.ps1
+++ create-list.ps1
@@ -9,18 +9,44 @@ if ( $list -eq $null ) {
     $list = Get-PnPList -Identity $LIST_NAME
 }
 
+# Rename display name for "TItle"
 Set-PnPField -List $list -Identity Title -Values @{ Title = "TPName"}
-Add-PnPField -DisplayName "TPID" -InternalName "TPID" -Type Text -List $list -AddToDefaultView 
-Add-PnPField -DisplayName ATU -InternalName ATU -List $list -Type Text -AddToDefaultView
-Add-PnPField -DisplayName Territory -InternalName Territory -List $list -Type Text -AddToDefaultView
-Add-PnPField -DisplayName "State" -InternalName "State" -List $list -Type Text -AddToDefaultView
-Add-PnPField -DisplayName "AE" -InternalName "AE" -List $list -Type User -AddToDefaultView
-Add-PnPField -DisplayName "ATS" -InternalName "ATS" -List $list -Type User -AddToDefaultView
-Add-PnPField -DisplayName "CSM" -InternalName "CSM" -List $list -Type User -AddToDefaultView
-Add-PnPField -DisplayName "VCSM" -InternalName "VCSM" -List $list -Type User -AddToDefaultView
-Add-PnPField -DisplayName "ACSM" -InternalName "ACSM" -List $list -Type User -AddToDefaultView
 
-# Multi-User fields must use XML for options.
+# TPID
+Add-PnPField -DisplayName "TPID" -InternalName "TPID" -Type Text -List $list -AddToDefaultView 
+
+# Index TPID and enforce required &uniqueness
+Set-PnPField -List $list -Identity TPID -Values @{ Indexed = $true; Required = $true }
+Set-PnPField -List $list -Identity TPID -Values @{ EnforceUniqueValues = $true }
+
+# ATU
+Add-PnPField -DisplayName ATU -InternalName ATU -List $list -Type Text -AddToDefaultView
+
+# Territory
+Add-PnPField -DisplayName Territory -InternalName Territory -List $list -Type Text -AddToDefaultView
+
+# State/Province
+Add-PnPField -DisplayName "State" -InternalName "State" -List $list -Type Text -AddToDefaultView
+
+# Account Executive
+Add-PnPField -DisplayName "AE" -InternalName "AE" -List $list -Type User -AddToDefaultView
+
+# Account Technology Strategist
+Add-PnPField -DisplayName "ATS" -InternalName "ATS" -List $list -Type User -AddToDefaultView
+
+# Primary CSM
+Add-PnPField -DisplayName "CSM" -InternalName "CSM" -List $list -Type User -AddToDefaultView
+
+# Primary CSM
+Add-PnPField -DisplayName "iCSM" -InternalName "iCSM" -List $list -Type User -AddToDefaultView
+
+# Voice CSM
+Add-PnPField -DisplayName "Voice CSM" -InternalName "VoiceCSM" -List $list -Type User -AddToDefaultView
+
+# App/Plat CSM
+Add-PnPField -DisplayName "App CSM" -InternalName "AppCSM" -List $list -Type User -AddToDefaultView
+
+# CSAM: Multi-User fields must use XML for options.
 $xml = @'
 <Field 
     Type="UserMulti" 
