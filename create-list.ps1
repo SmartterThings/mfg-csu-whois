81a5ff793f7e92f8333e781d6b8922759a11fe57
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-08-17 10:46:28 -0500

    Removed field prefix Added fields

--- create-list.ps1
+++ create-list.ps1
@@ -5,14 +5,22 @@ $FIELD_PREFIX = "mfg_"
 
 $list = New-PnPList -Title $LIST_NAME -Template GenericList -Url "Lists/$LIST_NAME"
 
-Add-PnPField -DisplayName "TPID" -InternalName ($FIELD_PREFIX + "TPID") -Type Text -List $list -AddToDefaultView 
-Add-PnPField -DisplayName "State" -InternalName ($FIELD_PREFIX + "State") -List $list -Type Text -AddToDefaultView
-Add-PnPField -DisplayName "AE" -InternalName ($FIELD_PREFIX + "AE" ) -List $list -Type User -AddToDefaultView
-Add-PnPField -DisplayName "ATS" -InternalName ($FIELD_PREFIX + "ATS" ) -List $list -Type User -AddToDefaultView
-Add-PnPField -DisplayName "CSM" -InternalName ($FIELD_PREFIX + "CSM") -List $list -Type User -AddToDefaultView
-Add-PnPField -DisplayName "VCSM" -InternalName ($FIELD_PREFIX + "VCSM") -List $list -Type User -AddToDefaultView
-Add-PnPField -DisplayName "ACSM" -InternalName ($FIELD_PREFIX + "ACSM") -List $list -Type User -AddToDefaultView
+if ( $list -eq $null ) {
+    $list = Get-PnPList -Identity $LIST_NAME
+}
 
+Set-PnPField -List $list -Identity Title -Values @{ Title = "TPName"}
+Add-PnPField -DisplayName "TPID" -InternalName "TPID" -Type Text -List $list -AddToDefaultView 
+Add-PnPField -DisplayName ATU -InternalName ATU -List $list -Type Text -AddToDefaultView
+Add-PnPField -DisplayName Territory -InternalName Territory -List $list -Type Text -AddToDefaultView
+Add-PnPField -DisplayName "State" -InternalName "State" -List $list -Type Text -AddToDefaultView
+Add-PnPField -DisplayName "AE" -InternalName "AE" -List $list -Type User -AddToDefaultView
+Add-PnPField -DisplayName "ATS" -InternalName "ATS" -List $list -Type User -AddToDefaultView
+Add-PnPField -DisplayName "CSM" -InternalName "CSM" -List $list -Type User -AddToDefaultView
+Add-PnPField -DisplayName "VCSM" -InternalName "VCSM" -List $list -Type User -AddToDefaultView
+Add-PnPField -DisplayName "ACSM" -InternalName "ACSM" -List $list -Type User -AddToDefaultView
+
+# Multi-User fields must use XML for options.
 $xml = @'
 <Field 
     Type="UserMulti" 
@@ -22,8 +30,8 @@ $xml = @'
     ID="{3a6091de-45e5-4022-be96-9f78d833d507}" 
     ShowField="Display Name" 
     UserSelectionMode="PeopleOnly" 
-    StaticName="mfg_CSAM" 
-    Name="mfg_CSAM" 
+    StaticName="CSAM" 
+    Name="CSAM" 
     Mult="TRUE" />
 '@
 Add-PnPFieldFromXml -List $list -FieldXml $xml
